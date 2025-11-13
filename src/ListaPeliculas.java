3b077ba51683222cc760d21cba494d4c312973bd
VGE <estebitanvaras@gmail.com>
2025-11-13 19:32:50 +0100

    Update the first commit

--- src/ListaPeliculas.java
+++ src/ListaPeliculas.java
@@ -1,5 +1,5 @@
 public class ListaPeliculas {
-    public static void main(String[] args) {
-        System.out.println("Peliculas de Netflix");
+    public static void main() {
+        System.out.println("Hello World");
     }
 }
